28e0523c2ef89619aba9d9ef5466e414a82926cb
Jessica Morgan <jessicamorgan@JessicasMBP104.lan>
2018-04-14 09:34:43 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     // Function that starts the trivia game when the start button is clicked and starts the timer
     $("body").on("click", ".start-button", function(event){
         generateHTML();
-        timerWrapper();
+        timer();
     
     }); 
     
@@ -21,67 +21,71 @@ $(document).ready(function() {
         selectedAnswer = $(this).text();
         if(selectedAnswer === correctAnswers[questionCounter]) {
             console.log("CORRECT!");
-            clearInterval(theClock);
-            generateWin();
+            clearInterval(clock);
+            win();
         }
         else {
             console.log("INCORRECT!");
-            clearInterval(theClock);
-            generateLoss();
+            clearInterval(clock);
+            loss();
         }
     });
     
     $("body").on("click", ".reset-button", function(event){
-        // clickSound.play();
-        resetGame();
-    }); // Closes reset-button click
+        reset();
+    }); 
     
-    });  //  Closes jQuery wrapper
+    });  
     
-    function generateLossDueToTimeOut() {
-        unansweredTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
-        $(".questionArea").html(gameHTML);
+    // Create function that generates a loss due to running out of time
+    function timeOutLoss() {
+        unansweredCounter++;
+        theGame = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+        $(".questionArea").html(theGame);
         setTimeout(wait, 3000);  
     }
     
-    function generateWin() {
-        correctTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
-        $(".questionArea").html(gameHTML);
+    // Create function that generates a loss due to running out of time
+    function win() {
+        correctCounter++;
+        theGame = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + images[questionCounter];
+        $(".questionArea").html(theGame);
         setTimeout(wait, 3000);  
     }
     
-    function generateLoss() {
-        incorrectTally++;
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='./assets/images/wronganswer.webp'>";
-        $(".questionArea").html(gameHTML);
+    // Create function that generates aa loss
+    function loss() {
+        incorrectCounter++;
+        theGame = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='./assets/images/wronganswer.webp'>";
+        $(".questionArea").html(theGame);
         setTimeout(wait, 3000); 
     }
     
+    // Generates the game HTML
     function generateHTML() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
-        $(".questionArea").html(gameHTML);
+        theGame = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questions[questionCounter] + "</p><p class='first-answer answer'>A. " + answers[questionCounter][0] + "</p><p class='answer'>B. "+answers[questionCounter][1]+"</p><p class='answer'>C. "+answers[questionCounter][2]+"</p><p class='answer'>D. "+answers[questionCounter][3]+"</p>";
+        $(".questionArea").html(theGame);
     }
     
     function wait() {
-        if (questionCounter < 2) {
+        if (questionCounter < 3) {
         questionCounter++;
         generateHTML();
         counter = 30;
-        timerWrapper();
+        timer();
         }
         else {
             finalScreen();
         }
     }
     
-    function timerWrapper() {
-        theClock = setInterval(thirtySeconds, 1000);
+    // Timer function
+    function timer() {
+        clock = setInterval(thirtySeconds, 1000);
         function thirtySeconds() {
             if (counter === 0) {
-                clearInterval(theClock);
-                generateLossDueToTimeOut();
+                clearInterval(clock);
+                timeOutLoss();
             }
             if (counter > 0) {
                 counter--;
@@ -91,31 +95,32 @@ $(document).ready(function() {
     }
     
     function finalScreen() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
-        $(".questionArea").html(gameHTML);
+        theGame = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctCounter + "</p>" + "<p>Wrong Answers: " + incorrectCounter + "</p>" + "<p>Unanswered: " + unansweredCounter + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+        $(".questionArea").html(theGame);
     }
     
-    function resetGame() {
+    // Reset the score, questions, game when the reset button is selected
+    function reset() {
         questionCounter = 0;
-        correctTally = 0;
-        incorrectTally = 0;
-        unansweredTally = 0;
+        correctCounter = 0;
+        incorrectCounter = 0;
+        unansweredCounter = 0;
         counter = 30;
         generateHTML();
-        timerWrapper();
+        timer();
     }
     
+    // Variables 
     var startScreen;
-    var gameHTML;
+    var theGame;
     var counter = 30;
-    var questionArray = ["What was the name of the TV newsman that Blanche dated?", "How did the girls know that Sophia had been driving?", "Sopia sat on a park bench with a man. What was his name and what was wrong with him?"];
-    var answerArray = [["Jerry Kennedy", "Bob Jones", "Jerry Orbach", "Richard Brown"], ["The car was missing", "The phonebook was missing", "She got arrested for speeding", "She wrecked the car"], ["Alfred, Parkinson's", "Alvin, Alzheimer's", "Adam, Dementia", "Arnold, Cancer"]];    
-    var imageArray = ["<img class='center-block img-right' src='./assets/images/jerrykennedy.jpg'>", "<img class='center-block img-right' src='./assets/images/phonebook.jpg'>", "<img class='center-block img-right' src='./assets/images/alvin.jpg'>"];
-    var correctAnswers = ["A. Jerry Kennedy", "B. The phonebook was missing", "B. Alvin, Alzheimer's"];
+    var questions = ["What was the name of the TV newsman that Blanche dated?", "How did the girls know that Sophia had been driving?", "Sopia sat on a park bench with a man. What was his name and what was wrong with him?", "Where was Rose from?"];
+    var answers = [["Jerry Kennedy", "Bob Jones", "Jerry Orbach", "Richard Brown"], ["The car was missing", "The phonebook was missing", "She got arrested for speeding", "She wrecked the car"], ["Alfred, Parkinson's", "Alvin, Alzheimer's", "Adam, Dementia", "Arnold, Cancer"], ["Houston", "St. Olaf", "Denver", "New York"]];    
+    var images = ["<img class='center-block img-right' src='./assets/images/jerrykennedy.jpg'>", "<img class='center-block img-right' src='./assets/images/phonebook.jpg'>", "<img class='center-block img-right' src='./assets/images/alvin.jpg'>", "<img class='center-block img-right' src='./assets/images/rose.jpeg'>"];
+    var correctAnswers = ["A. Jerry Kennedy", "B. The phonebook was missing", "B. Alvin, Alzheimer's", "B. St. Olaf"];
     var questionCounter = 0;
-    var selecterAnswer;
-    var theClock;
-    var correctTally = 0;
-    var incorrectTally = 0;
-    var unansweredTally = 0;
-    // var clickSound = new Audio("sound/button-click.mp3");
+    var selectAns;
+    var clock;
+    var correctCounter = 0;
+    var incorrectCounter = 0;
+    var unansweredCounter = 0;
